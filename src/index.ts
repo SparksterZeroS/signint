12ea29bc08c47f8722323de2f216992940911c90
Anthony I. Jackson Jr. <contact@anthonyijacksonjr.xyz>
2026-02-11 17:53:42 -0500

    Project refactoring.
    I forgot to stage "src/index.ts" in the previous commit like an idiot.

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,23 @@
-const UINT8_MIN: number = 0;
-const UINT8_MAX: number = (2**8) - 1;
-const UINT16_MIN: number = 0;
-const UINT16_MAX: number = (2**16) - 1;
-const UINT32_MIN: number = 0;
-const UINT32_MAX: number = (2**32) - 1;
-const UINT64_MIN: number = 0;
-const UINT64_MAX: number = (2**64) - 1;
-const INT8_MIN: number = -(2**7);
-const INT8_MAX: number = (2**7) - 1;
-const INT16_MIN: number = -(2**15);
-const INT16_MAX: number = 2**15;
-const INT32_MIN: number = -(2**31);
-const INT32_MAX: number = (2**31) - 1;
-const INT64_MIN: bigint = -(2n**63n);
-const INT64_MAX: bigint = (2n**63n) - 1n;
+namespace SignInt {
+    export const UINT8_MIN: number = 0;
+    export const UINT8_MAX: number = 2 ** 8 - 1;
+    export const UINT16_MIN: number = 0;
+    export const UINT16_MAX: number = 2 ** 16 - 1;
+    export const UINT32_MIN: number = 0;
+    export const UINT32_MAX: number = 2 ** 32 - 1;
+    export const UINT64_MIN: number = 0;
+    export const UINT64_MAX: number = 2 ** 64 - 1;
+    export const INT8_MIN: number = -(2 ** 7);
+    export const INT8_MAX: number = 2 ** 7 - 1;
+    export const INT16_MIN: number = -(2 ** 15);
+    export const INT16_MAX: number = 2 ** 15;
+    export const INT32_MIN: number = -(2 ** 31);
+    export const INT32_MAX: number = 2 ** 31 - 1;
 
-const unsignedToSigned8 = (int8: number): number => Int8Array.from(Uint8Array.of(int8))[0];
-const signedToUnsigned8 = (uint8: number): number => Uint8Array.from(Int8Array.of(uint8))[0];
-const unsignedToSigned16 = (int16: number): number => Int16Array.from(Uint16Array.of(int16))[0];
-const signedToUnsigned16 = (uint16: number): number => Uint16Array.from(Int16Array.of(uint16))[0];
-const unsignedToSigned32 = (int32: number): number => Int32Array.from(Uint32Array.of(int32))[0];
-const signedToUnsigned32 = (uint32: number): number => Uint32Array.from(Int32Array.of(uint32))[0];
-const unsignedToSigned64 = (int64: bigint): bigint => BigInt64Array.from(BigUint64Array.of(int64))[0];
-const signedToUnsigned64 = (uint64: bigint): bigint => BigUint64Array.from(BigInt64Array.of(uint64))[0];
-
-export default {
-    UINT8_MIN,
-    UINT8_MAX,
-    UINT16_MIN,
-    UINT16_MAX,
-    UINT32_MIN,
-    UINT32_MAX,
-    UINT64_MIN,
-    UINT64_MAX,
-    INT8_MIN,
-    INT8_MAX,
-    INT16_MIN,
-    INT16_MAX,
-    INT32_MIN,
-    INT32_MAX,
-    INT64_MIN,
-    INT64_MAX,
-    signedToUnsigned8,
-    unsignedToSigned8,
-    signedToUnsigned16,
-    unsignedToSigned16,
-    signedToUnsigned32,
-    unsignedToSigned32,
-    signedToUnsigned64,
-    unsignedToSigned64
+    export const unsignedToSigned8 = (int8: number): number => Int8Array.from(Uint8Array.of(int8))[0];
+    export const signedToUnsigned8 = (uint8: number): number => Uint8Array.from(Int8Array.of(uint8))[0];
+    export const unsignedToSigned16 = (int16: number): number => Int16Array.from(Uint16Array.of(int16))[0];
+    export const signedToUnsigned16 = (uint16: number): number => Uint16Array.from(Int16Array.of(uint16))[0];
+    export const unsignedToSigned32 = (int32: number): number => Int32Array.from(Uint32Array.of(int32))[0];
+    export const signedToUnsigned32 = (uint32: number): number => Uint32Array.from(Int32Array.of(uint32))[0];
 }
