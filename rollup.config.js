c550cd8c52b2cfcc68f3ddae11d83bf8df613ce5
Anthony I. Jackson Jr. <contact@anthonyijacksonjr.xyz>
2026-02-13 02:58:50 -0500

    Build system update and documentation.
    I decided to change the way the dts is generated, opting to use a rollup plugin instead of the typescript compiler or standalone dts-bundle-generator.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import terser from "@rollup/plugin-terser";
 import { babel } from "@rollup/plugin-babel";
+import { dts } from "rollup-plugin-dts";
 
 import * as Path from "node:path";
 import * as Filesystem from "node:fs";
@@ -8,31 +9,38 @@ import * as URL from "node:url";
 const projectDirectory = Path.dirname(URL.fileURLToPath(import.meta.url));
 const srcDir = Path.join(projectDirectory, "src");
 const distDir = Path.join(projectDirectory, "dist");
-
 const pkg = JSON.parse(Filesystem.readFileSync(Path.join(projectDirectory, "package.json"), "utf-8"));
 
-const projectName = pkg.name;
-
-/** @type {import("rollup").RollupOptions} */
-export default {
-    input: Path.join(srcDir, "index.ts"),
-    output: [
-        {
-            name: projectName,
-            file: Path.join(distDir, `${projectName}.js`),
-            format: "umd",
-            sourcemap: true,
+/** @type {import("rollup").RollupOptions[]} */
+export default [
+    {
+        input: Path.join(srcDir, "index.ts"),
+        output: [
+            {
+                name: pkg.libraryName,
+                file: Path.join(distDir, `${pkg.fileName}.js`),
+                format: "umd",
+                sourcemap: true,
+            },
+            {
+                name: pkg.libraryName,
+                file: Path.join(distDir, `${pkg.fileName}.min.js`),
+                format: "umd",
+                plugins: [terser()],
+            },
+        ],
+        plugins: [
+            babel({
+                extensions: [".ts"],
+            }),
+        ],
+    },
+    {
+        input: Path.join(srcDir, "index.ts"),
+        output: {
+            file: Path.join(distDir, `${pkg.fileName}.d.ts`),
+            format: "umd"
         },
-        {
-            name: projectName,
-            file: Path.join(distDir, `${projectName}.min.js`),
-            format: "umd",
-            plugins: [terser()]
-        },
-    ],
-    plugins: [
-        babel({
-            extensions: [".ts"],
-        }),
-    ],
-};
+        plugins: [dts()],
+    },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,157 @@
-export namespace SignInt {
-    export const UINT8_MIN: number = 0;
-    export const UINT8_MAX: number = 2 ** 8 - 1;
-    export const UINT16_MIN: number = 0;
-    export const UINT16_MAX: number = 2 ** 16 - 1;
-    export const UINT32_MIN: number = 0;
-    export const UINT32_MAX: number = 2 ** 32 - 1;
-    export const UINT64_MIN: number = 0;
-    export const UINT64_MAX: number = 2 ** 64 - 1;
-    export const INT8_MIN: number = -(2 ** 7);
-    export const INT8_MAX: number = 2 ** 7 - 1;
-    export const INT16_MIN: number = -(2 ** 15);
-    export const INT16_MAX: number = 2 ** 15;
-    export const INT32_MIN: number = -(2 ** 31);
-    export const INT32_MAX: number = 2 ** 31 - 1;
+/**
+ * The minimum value an unsigned byte can be.
+ * @type {number}
+ */
+export const UINT8_MIN: number = 0;
 
-    function clamp(value: number, minimum: number, maximum: number): number {
-        return value < minimum ? minimum : value > maximum ? maximum : value;
-    }
+/**
+ * The maximum value an unsigned byte can be.
+ * @type {number}
+ */
+export const UINT8_MAX: number = 2 ** 8 - 1;
 
-    export function unsignedToSigned8(value: number): number {
-        let clamped: number = clamp(value, UINT8_MIN, UINT8_MAX);
-        let converted: number | undefined = Int8Array.from(Uint8Array.of(clamped))[0];
-        return converted !== undefined ? converted : NaN;
-    }
+/**
+ * The minimum value an unsigned word can be.
+ * @type {number}
+ */
+export const UINT16_MIN: number = 0;
 
-    export function signedToUnsigned8(value: number): number {
-        let clamped: number = clamp(value, INT8_MIN, INT8_MAX);
-        let converted: number | undefined = Uint8Array.from(Int8Array.of(clamped))[0];
-        return converted !== undefined ? converted : NaN;
-    }
+/**
+ * The maximum value an unsigned word can be.
+ * @type {number}
+ */
+export const UINT16_MAX: number = 2 ** 16 - 1;
 
-    export function unsignedToSigned16(value: number): number {
-        let clamped: number = clamp(value, UINT16_MIN, UINT16_MAX);
-        let converted: number | undefined = Int16Array.from(Uint16Array.of(clamped))[0];
-        return converted !== undefined ? converted : NaN;
-    }
+/**
+ * The minimum value an unsigned double word can be.
+ * @type {number}
+ */
+export const UINT32_MIN: number = 0;
 
-    export function signedToUnsigned16(value: number): number {
-        let clamped: number = clamp(value, INT16_MIN, INT16_MAX);
-        let converted: number | undefined = Uint16Array.from(Int16Array.of(clamped))[0];
-        return converted !== undefined ? converted : NaN;
-    }
+/**
+ * The maximum value an unsigned double word can be.
+ * @type {number}
+ */
+export const UINT32_MAX: number = 2 ** 32 - 1;
 
-    export function unsignedToSigned32(value: number): number {
-        let clamped: number = clamp(value, UINT16_MIN, UINT16_MAX);
-        let converted: number | undefined = Int32Array.from(Uint32Array.of(clamped))[0];
-        return converted !== undefined ? converted : NaN;
-    }
+/**
+ * The minimum value an signed byte can be.
+ * @type {number}
+ */
+export const INT8_MIN: number = -(2 ** 7);
 
-    export function signedToUnsigned32(value: number): number {
-        let clamped: number = clamp(value, INT32_MIN, INT32_MAX);
-        let converted: number | undefined = Uint32Array.from(Int32Array.of(clamped))[0];
-        return converted !== undefined ? converted : NaN;
-    }
+/**
+ * The maximum value an signed byte can be.
+ * @type {number}
+ */
+export const INT8_MAX: number = 2 ** 7 - 1;
+
+/**
+ * The minimum value an signed word can be.
+ * @type {number}
+ */
+export const INT16_MIN: number = -(2 ** 15);
+
+/**
+ * The maximum value an signed word can be.
+ * @type {number}
+ */
+export const INT16_MAX: number = 2 ** 15;
+
+/**
+ * The minimum value an signed double word can be.
+ * @type {number}
+ */
+export const INT32_MIN: number = -(2 ** 31);
+
+/**
+ * The maximum value an signed double word can be.
+ * @type {number}
+ */
+export const INT32_MAX: number = 2 ** 31 - 1;
+
+/**
+ * Clamps a number between a range.
+ * @param value The number to clamp.
+ * @param minimum The minimum value.
+ * @param maximum The maximum value.
+ * @returns
+ */
+// This function really only serves to prevent a bug when doing the conversions.
+// For the some reason, if the number is out of the range of the byte-width of whatever
+// integer is being converted, it causes some weird behavior.
+// So to prevent whatever is going on, we'll use this for now. And also because it wouldn't
+// hurt to.
+function clamp(value: number, minimum: number, maximum: number): number {
+    return value < minimum ? minimum : value > maximum ? maximum : value;
+}
+
+/**
+ * Converts a `number` representation of a unsigned byte into a signed representation.
+ * @param {number} value The unsigned byte to convert.
+ * @returns {number} A signed `number` or `NaN` on failure.
+ */
+export function unsignedToSigned8(value: number): number {
+    let clamped: number = clamp(value, UINT8_MIN, UINT8_MAX);
+    let converted: number | undefined = Int8Array.from(Uint8Array.of(clamped))[0];
+    return converted !== undefined ? converted : NaN;
+}
+
+/**
+ * Converts a `number` representation of a signed byte into a unsigned representation.
+ * @param {number} value The signed byte to convert.
+ * @returns {number} A unsigned `number` or `NaN` on failure.
+ */
+export function signedToUnsigned8(value: number): number {
+    let clamped: number = clamp(value, INT8_MIN, INT8_MAX);
+    let converted: number | undefined = Uint8Array.from(Int8Array.of(clamped))[0];
+    return converted !== undefined ? converted : NaN;
+}
+
+/**
+ * Converts a `number` representation of a unsigned word into a signed representation.  
+ * The endianness used is defined by the runtime. See [Endianness (MDN)](https://developer.mozilla.org/docs/Glossary/Endianness)
+ * @param {number} value The unsigned word to convert.
+ * @returns {number} A signed `number` or `NaN` on failure.
+ */
+export function unsignedToSigned16(value: number): number {
+    let clamped: number = clamp(value, UINT16_MIN, UINT16_MAX);
+    let converted: number | undefined = Int16Array.from(Uint16Array.of(clamped))[0];
+    return converted !== undefined ? converted : NaN;
+}
+
+/**
+ * Converts a `number` representation of a signed word into a unsigned representation.  
+ * The endianness used is defined by the runtime. See [Endianness (MDN)](https://developer.mozilla.org/docs/Glossary/Endianness)
+ * @param {number} value The signed word to convert.
+ * @returns {number} A unsigned `number` or `NaN` on failure.
+ */
+export function signedToUnsigned16(value: number): number {
+    let clamped: number = clamp(value, INT16_MIN, INT16_MAX);
+    let converted: number | undefined = Uint16Array.from(Int16Array.of(clamped))[0];
+    return converted !== undefined ? converted : NaN;
+}
+
+/**
+ * Converts a `number` representation of a unsigned double word into a signed representation.  
+ * The endianness used is defined by the runtime. See [Endianness (MDN)](https://developer.mozilla.org/docs/Glossary/Endianness)
+ * @param {number} value The unsigned double word to convert.
+ * @returns {number} A signed `number` or `NaN` on failure.
+ */
+export function unsignedToSigned32(value: number): number {
+    let clamped: number = clamp(value, UINT16_MIN, UINT16_MAX);
+    let converted: number | undefined = Int32Array.from(Uint32Array.of(clamped))[0];
+    return converted !== undefined ? converted : NaN;
+}
+
+/**
+ * Converts a `number` representation of a signed double into a unsigned representation.  
+ * The endianness used is defined by the runtime. See [Endianness (MDN)](https://developer.mozilla.org/docs/Glossary/Endianness)
+ * @param {number} value The signed double to convert.
+ * @returns {number} A unsigned `number` or `NaN` on failure.
+ */
+export function signedToUnsigned32(value: number): number {
+    let clamped: number = clamp(value, INT32_MIN, INT32_MAX);
+    let converted: number | undefined = Uint32Array.from(Int32Array.of(clamped))[0];
+    return converted !== undefined ? converted : NaN;
 }
